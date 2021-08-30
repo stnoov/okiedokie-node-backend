197e6ad4b7f21534f82c3fb44237918ab83d021f
Artem Sitnov <stnoov@gmail.com>
2021-08-30 21:19:11 +0300

    update check notification variable

--- controllers/payments.controller.js
+++ controllers/payments.controller.js
@@ -1,16 +1,7 @@
 require("dotenv").config();
 
 exports.handle_payments = (req, res) => {
-  const checkNotification =
-    req.body.notification_type &
-    req.body.operation_id &
-    req.body.amount &
-    req.body.currency &
-    req.body.datetime &
-    req.body.sender &
-    req.body.codepro &
-    process.env.NOTIFICATION_SECRET &
-    req.body.label;
+  const checkNotification = `${req.body.notification_type}&${req.body.operation_id}&${req.body.req.body.amount}&${req.body.req.body.currency}&${req.body.req.body.datetime}&${req.body.sender}&${req.body.codepro}&${process.env.NOTIFICATION_SECRET}&${req.body.label}&`;
   console.log("---------------------------");
   console.log("checkNotification: ", checkNotification);
   console.log("---------------------------");
