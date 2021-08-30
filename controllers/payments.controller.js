1f7fb4352e246c86cef9a20dd552420470f9e577
Artem Sitnov <stnoov@gmail.com>
2021-08-30 21:15:33 +0300

    update payments

--- controllers/payments.controller.js
+++ controllers/payments.controller.js
@@ -1,5 +1,17 @@
+require("dotenv").config();
+
 exports.handle_payments = (req, res) => {
+  const checkNotification =
+    req.body.notification_type &
+    req.body.operation_id &
+    req.body.amount &
+    req.body.currency &
+    req.body.datetime &
+    req.body.sender &
+    req.body.codepro &
+    process.env.NOTIFICATION_SECRET &
+    req.body.label;
   console.log("---------------------------");
-  console.log(req.body);
+  console.log("checkNotification: ", checkNotification);
   console.log("---------------------------");
 };
