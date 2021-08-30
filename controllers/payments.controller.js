ba7ed3655ac3351add1784a7cc8b78ad4679e3cc
Artem Sitnov <stnoov@gmail.com>
2021-08-30 21:34:18 +0300

    added sha1 library

--- controllers/payments.controller.js
+++ controllers/payments.controller.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-var crypto = require("crypto");
+var sha1 = require("sha1");
 
 exports.handle_payments = (req, res) => {
   const checkNotification = `${req.body.notification_type}&${req.body.operation_id}&${req.body.amount}&${req.body.currency}&${req.body.datetime}&${req.body.sender}&${req.body.codepro}&${process.env.NOTIFICATION_SECRET}&${req.body.label}`;
-  const sha1_hash = crypto.createHash(checkNotification);
-  console.log("sha1_hash: ", sha1_hash.digest("hex"));
+  const sha1_hash = sha1(checkNotification);
+  console.log("sha1_hash: ", sha1_hash);
   if (req.body.sha1_hash === sha1_hash) {
     return "SUCCESS";
   }
