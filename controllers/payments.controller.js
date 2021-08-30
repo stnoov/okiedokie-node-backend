0c2b6bbc3ace09264e101f9b65f7f2694d80b98b
Artem Sitnov <stnoov@gmail.com>
2021-08-30 21:30:38 +0300

    add sha1_hash of notification

--- controllers/payments.controller.js
+++ controllers/payments.controller.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
+var crypto = require("crypto");
 
 exports.handle_payments = (req, res) => {
-  const checkNotification = `${req.body.notification_type}&${req.body.operation_id}&${req.body.amount}&${req.body.currency}&${req.body.datetime}&${req.body.sender}&${req.body.codepro}&${process.env.NOTIFICATION_SECRET}&${req.body.label}&`;
-  console.log("---------------------------");
-  console.log("checkNotification: ", checkNotification);
-  console.log("---------------------------");
+  const checkNotification = `${req.body.notification_type}&${req.body.operation_id}&${req.body.amount}&${req.body.currency}&${req.body.datetime}&${req.body.sender}&${req.body.codepro}&${process.env.NOTIFICATION_SECRET}&${req.body.label}`;
+  const sha1_hash = crypto.createHash(checkNotification);
+  console.log("sha1_hash: ", sha1_hash.digest("hex"));
+  if (req.body.sha1_hash === sha1_hash) {
+    return "SUCCESS";
+  }
 };
