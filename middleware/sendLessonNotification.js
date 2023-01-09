8026f725a0e2a9874b55fb0ece8c6559ad5f958a
Artem Sitnov <stnoov@gmail.com>
2023-01-09 21:44:03 +0300

    update date format for notifications

--- middleware/sendLessonNotification.js
+++ middleware/sendLessonNotification.js
@@ -18,12 +18,12 @@ const sendLessonNotification = (locale, email, link, date, time) => {
           subject: "Ваше следующее мероприятие в OkieDokie! club",
           html:
             "<p>Вы успешно зарегистрировались на встречу в OkieDokie! club, которая состоится " +
-            new Date(date).toLocaleDateString() +
+            new Date(date).toLocaleDateString("uk-uk") +
             " в " +
             time +
             '<br/> Ссылка на подключение: <a href="' +
             link +
-            '">link</a> <br /> Так же, cсылку на подключение вы можете также найти на главной странице сайта, на картинке забронированного мероприятия, за 15 минут до его начала <br /> <br /> <br /> Warmest regards, <br />Your OkieDokie!</p>',
+            '">link</a> <br /> Так же, cсылку на подключение вы можете найти на главной странице сайта, на картинке забронированного мероприятия, за 15 минут до его начала <br /> <br /> <br /> Warmest regards, <br />Your OkieDokie!</p>',
         }
       : {
           from: "hello@okiedokie.club",
@@ -31,7 +31,7 @@ const sendLessonNotification = (locale, email, link, date, time) => {
           subject: "Your next OkieDokie! club meeting",
           html:
             "<p>You are successfully registered for the OkieDokie meeting on " +
-            new Date(date).toLocaleDateString() +
+            new Date(date).toLocaleDateString("uk-uk") +
             " at " +
             time +
             '<br/> Click here to join: <a href="' +
